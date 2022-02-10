6f33731d0822cd5b59b894fe4979a72bd0f98261
Igor Silva <igorsb3000@gmail.com>
2022-02-10 20:06:07 -0300

    Carrossel implementado nos Meus Favoritos

--- Projeto_03_ComparadorDeCartas/js/functions.js
+++ Projeto_03_ComparadorDeCartas/js/functions.js
@@ -2,8 +2,8 @@
 *   Author: Igor Silva Bento
 */
 
+// Guarda todas as cartas obtidas na API
 var objeto_carta;
-// >>>>>>>>> PODE REMOVER const quantidade_cartas = 50;
 
 function xhttpAssincrono(callBackFunction, type, value) {
     var xhttp = new XMLHttpRequest();;
@@ -24,12 +24,11 @@ function xhttpAssincrono(callBackFunction, type, value) {
     xhttp.send();
 }
 
-
 function atualizar_menu(){
     xhttpAssincrono(montar_select, 0, null);
 }
 
-
+// Funcao que atualiza os selects para exibir os nomes das cartas
 function montar_select(response){
     objeto_carta = JSON.parse(response);
     for(i=0; i<objeto_carta.data.length; i++){
@@ -43,6 +42,7 @@ function montar_select(response){
     }
 } 
 
+// Funcao responsável por exibir a imagem da carta, descrição e botões de favoritar e desfavoritar
 function exibir_carta(id_selecionado, numero){
     var imagem = document.createElement("img");
     imagem.setAttribute('src', objeto_carta.data[id_selecionado-1].card_images[0].image_url);
@@ -98,14 +98,12 @@ function exibir_carta(id_selecionado, numero){
     defesa.appendChild(label_defesa);
     texto = document.createTextNode(objeto_carta.data[id_selecionado-1].def);
     defesa.appendChild(texto);
-    
+
     if(numero == 1){
         fechar_board(1);
         fechar_descricao(1);
+        exibir_botoes(objeto_carta.data[id_selecionado-1].id, 1);
         document.getElementById("imagem_carta_1").appendChild(imagem);
-        exibir_botao_favoritar(objeto_carta.data[id_selecionado-1].id, 1);
-        exibir_botao_desfavoritar(objeto_carta.data[id_selecionado-1].id, 1);
-        //document.getElementById("descricao_1").appendChild(descricao);
         document.getElementById("descricao_1").appendChild(nome);
         document.getElementById("descricao_1").appendChild(tipo);
         document.getElementById("descricao_1").appendChild(nivel);
@@ -116,9 +114,8 @@ function exibir_carta(id_selecionado, numero){
     }else if(numero == 2){
         fechar_board(2);
         fechar_descricao(2);
+        exibir_botoes(objeto_carta.data[id_selecionado-1].id, 2);
         document.getElementById("imagem_carta_2").appendChild(imagem);
-        exibir_botao_favoritar(objeto_carta.data[id_selecionado-1].id, 2);
-        exibir_botao_desfavoritar(objeto_carta.data[id_selecionado-1].id, 2);
         document.getElementById("descricao_2").appendChild(nome);
         document.getElementById("descricao_2").appendChild(tipo);
         document.getElementById("descricao_2").appendChild(nivel);
@@ -129,61 +126,41 @@ function exibir_carta(id_selecionado, numero){
     
 }
 
-function exibir_botao_favoritar(id_selecionado, select){
+// Funcao que verifica se a carta está entre as favoritas e com base nisso exibe os botões favoritar e desfavoritar
+function exibir_botoes(id_selecionado, select){
+    let achou_carta = false;
     for(i=0; i<localStorage.length; i++){
         let id_carta = localStorage.getItem(localStorage.key(i));
         let objeto = JSON.parse(id_carta);
         if(objeto.id == id_selecionado){
-            if(select == 1){
-                document.getElementById("botao_1_favorito").disabled = true;
-                break;
-            }
-            else if(select == 2){
-                document.getElementById("botao_2_favorito").disabled = true;
-                break;
-            }
-                
-        }else{
-            if(select == 1){
-                document.getElementById("botao_1_favorito").disabled = false;
-                break;
-            }
-            else if(select == 2){
-                document.getElementById("botao_2_favorito").disabled = false;
-                break;
-            }
-                
+            achou_carta = true;      
+        }            
+    } // fechando for
+    // Achou a carta, ela é uma das favoritas
+    if(achou_carta){
+        if(select == 1){
+            document.getElementById("botao_1_favorito").disabled = true;
+            document.getElementById("botao_1_desfavoritar").disabled = false;
+        }
+        else if(select == 2){
+            document.getElementById("botao_2_favorito").disabled = true;
+            document.getElementById("botao_2_desfavoritar").disabled = false;
+        }
+    }else{
+        if(select == 1){
+            document.getElementById("botao_1_favorito").disabled = false;
+            document.getElementById("botao_1_desfavoritar").disabled = true;
+        }
+        else if(select == 2){
+            document.getElementById("botao_2_favorito").disabled = false;
+            document.getElementById("botao_2_desfavoritar").disabled = true;
         }
     }
+    
+    
 }
 
-function exibir_botao_desfavoritar(id_selecionado, select){
-    for(i=0; i<localStorage.length; i++){
-        let id_carta = localStorage.getItem(localStorage.key(i));
-        let objeto = JSON.parse(id_carta);
-        if(objeto.id == id_selecionado){
-            if(select == 1){
-                document.getElementById("botao_1_desfavoritar").disabled = false;
-                break;
-            }
-            else if(select == 2){
-                document.getElementById("botao_2_desfavoritar").disabled = false;
-                break;
-            }
-        }else{
-            if(select == 1){
-                document.getElementById("botao_1_desfavoritar").disabled = true;
-                break;
-            }
-            else if(select == 2){
-                document.getElementById("botao_2_desfavoritar").disabled = true;
-                break;
-            }
-                
-        }
-    }
-}
-
+// Funcao que remove a carta dos meus favoritos
 function deletar_favorito(id_selecionado, select){
     for(i=0; i<objeto_carta.data.length; i++){
         if(objeto_carta.data[i].id == objeto_carta.data[id_selecionado-1].id){
@@ -199,10 +176,11 @@ function deletar_favorito(id_selecionado, select){
             }
         }
     }
+    exibir_favoritos();
 }
 
 
-
+// Funcao que adiciona a carta aos meus favoritos
 function armazenar_favorito(id_selecionado, select){
     localStorage.setItem(objeto_carta.data[id_selecionado-1].id, JSON.stringify(objeto_carta.data[id_selecionado-1] ));
     if(select == 1){
@@ -214,27 +192,36 @@ function armazenar_favorito(id_selecionado, select){
         document.getElementById("botao_2_desfavoritar").disabled = false;
         document.getElementById("botao_2_favorito").disabled = true;
     }
+    exibir_favoritos();
         
 }
 
+// Funcao que busca do LocalStorage as cartas favoritas e exibe na tela
 function exibir_favoritos(){
     fechar_favoritos();
     document.getElementById("fechar_favoritos").disabled = false;
     for(i=0; i<localStorage.length; i++){
+        var div = document.createElement("div");
+        div.className = 'carousel-item';
+        if(i==0)
+            div.className = 'carousel-item active';
         var id_carta = localStorage.getItem(localStorage.key(i));
         var objeto = JSON.parse(id_carta);
         try{
             var imagem = document.createElement("img");
+            imagem.className = 'center-block';
             imagem.setAttribute('src', objeto.card_images[0].image_url);
             imagem.width = 250;
             imagem.height = 350;
-            document.getElementById("favoritos").appendChild(imagem);
+            div.appendChild(imagem);
+            document.getElementById("favoritos").appendChild(div);
         }catch(e){
             console.log(e);
         }
     }
 }
 
+// Funcao que remove da tela as carta exibidas nos meus favoritos
 function fechar_favoritos(){
     let Node = document.getElementById("favoritos");
     while(Node.firstChild){
@@ -243,6 +230,7 @@ function fechar_favoritos(){
     document.getElementById("fechar_favoritos").disabled = true;
 }
 
+// Funcao que remove da tela a imagem de uma carta
 function fechar_board(numero){
     let Node;
     if(numero == 1){
@@ -255,6 +243,7 @@ function fechar_board(numero){
     } 
 }
 
+// Funcao que remove a descricao da carta da tela
 function fechar_descricao(numero){
     let Node;
     if(numero == 1){
@@ -268,8 +257,7 @@ function fechar_descricao(numero){
 }
 
 
-
-
+// Funcao que gera o gráfico com base nas cartas selecionadas nos selects
 function drawVisualization(id_selecionado_1, id_selecionado_2) {
     // Some raw data (not necessarily accurate)
     let carta_1 = null;
